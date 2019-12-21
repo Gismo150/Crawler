5f55d524d9600289b241086d4e6de3ca84ebd5f1
Gismo150 <braun.daniel@hotmail.de>
2019-12-21 17:51:23 +0100

    added simple logging collecting data for eval

--- src/main/MainCrawler.java
+++ src/main/MainCrawler.java
@@ -1,4 +1,9 @@
 package main;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.*;
+
 /**
  * Main entry point of the whole program
  *
@@ -7,9 +12,30 @@ package main;
 public class MainCrawler {
 
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        logger.setLevel(Level.ALL);
+        Handler handler = null;
+        try {
+            handler = new FileHandler("Crawler_Log", true);
+            handler.setLevel(Level.ALL);
+            logger.addHandler(handler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.setUseParentHandlers(false);
+        logger.info("------------------------------------------");
+        logger.config("CONFIGURATION:");
+        logger.config("Language: " + Config.LANGUAGE);
+        logger.config("Last pushed date: " + Config.LASTPUSHEDDATE);
+        logger.config("Stars decrease amount: " + Config.STARSDECREASEAMOUNT);
+        logger.config("Build system: " + Config.BUILDSYSTEM.toString());
+        logger.config("Filepath: " + Config.FILEPATH);
+        logger.info("------------------------------------------");
 
+
+        long startTime = System.nanoTime();
         //Init crawler with configuration.
-        GitHubCrawler crawler = new GitHubCrawler(Config.LANGUAGE, Config.LASTPUSHEDDATE, Config.STARSDECREASEAMOUNT, Config.BUILDSYSTEM, Config.OAUTHTOKEN);
+        GitHubCrawler crawler = new GitHubCrawler(Config.LANGUAGE, Config.LASTPUSHEDDATE, Config.STARSDECREASEAMOUNT, Config.BUILDSYSTEM, Config.OAUTHTOKEN, logger, startTime);
         //Start the crawler.
         crawler.run();
 
