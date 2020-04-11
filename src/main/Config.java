1005c1b03fa0b529df46a66ad77c12d2d1d2072c
Ali <alibozna@gmail.com>
2020-04-11 02:18:39 +0200

    Replace common-cli with jcommander, because common-cli didn't support sub-commands

--- src/main/Config.java
+++ src/main/Config.java
@@ -6,14 +6,14 @@ import utils.PropertyFileReader;
 
 public class Config {
 
-    public static final String LANGUAGE = PropertyFileReader.getInstance().getProperty(EConfig.LANGUAGE);
-    public static final String LASTPUSHEDDATE = PropertyFileReader.getInstance().getProperty(EConfig.LASTPUSHEDDATE);
-    public static final String STARSDECREASEAMOUNT = PropertyFileReader.getInstance().getProperty(EConfig.STARSDECREASEAMOUNT);
-    public static final BuildSystem BUILDSYSTEM = BuildSystem.getBuildType(PropertyFileReader.getInstance().getProperty(EConfig.BUILDSYSTEM));
-    public static final String OAUTHTOKEN = PropertyFileReader.getInstance().getProperty(EConfig.OAUTHTOKEN);
-    public static final String FILEPATH = PropertyFileReader.getInstance().getProperty(EConfig.FILEPATH);
-    public static final String JSONFILENAME = "repositories.json";
-    public static final String CUSTOMFILE = PropertyFileReader.getInstance().getProperty(EConfig.CUSTOMFILE);
+    public static final String LANGUAGE = null;//PropertyFileReader.getInstance().getProperty(EConfig.LANGUAGE);
+    public static final String LASTPUSHEDDATE = null;//PropertyFileReader.getInstance().getProperty(EConfig.LASTPUSHEDDATE);
+    public static final String STARSDECREASEAMOUNT = null;//PropertyFileReader.getInstance().getProperty(EConfig.STARSDECREASEAMOUNT);
+    public static final BuildSystem BUILDSYSTEM = null;//BuildSystem.getBuildType(PropertyFileReader.getInstance().getProperty(EConfig.BUILDSYSTEM));
+    public static final String OAUTHTOKEN = null;//PropertyFileReader.getInstance().getProperty(EConfig.OAUTHTOKEN);
+    public static final String FILEPATH = null;//PropertyFileReader.getInstance().getProperty(EConfig.FILEPATH);
+    public static final String JSONFILENAME = null;//"repositories.json";
+    public static final String CUSTOMFILE = null;//PropertyFileReader.getInstance().getProperty(EConfig.CUSTOMFILE);
 
 
 }
--- src/main/MainCrawler.java
+++ src/main/MainCrawler.java
@@ -15,6 +15,5 @@ public class MainCrawler {
         GitHubCrawler crawler = new GitHubCrawler(config);
         //Start the crawler.
         crawler.run();
-
     }
 }
